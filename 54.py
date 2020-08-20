26c6c6645d579062c91fb25f5994ef44014abf94
Micah Yong <micahtyong@gmail.com>
2020-08-19 17:30:25 -0700

    SDgeG

--- 54.py
+++ 54.py
@@ -1,33 +1,34 @@
 # Source: https://leetcode.com/problems/spiral-matrix/
 
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
-    # spiral matrix: right (dx=1, dy=0) -> down (dx=0, dy=-1) -> left (dx=-1, dy=0) -> up (dx=0, dy=1) -> right ...    
-    # m x n matrix
-    # output: all the elements in the spiral order
-    
-    
+
+        # spiral matrix: right (dx=1, dy=0) -> down (dx=0, dy=-1) -> left (dx=-1, dy=0) -> up (dx=0, dy=1) -> right ...
+        # m x n matrix
+        # output: all the elements in the spiral order
+
         res = []
+
         def goRight():
             if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
                 return
             res.extend(matrix.pop(0))
             goDown()
-        
+
         def goDown():
             if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
                 return
             rightMostCol = [row.pop() for row in matrix]
             res.extend(rightMostCol)
             goLeft()
-        
+
         def goLeft():
             if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
                 return
             res.extend(reversed(matrix.pop()))
             goUp()
-            
+
         def goUp():
             if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
                 return
@@ -38,11 +39,43 @@ class Solution:
             goRight()
         goRight()
         return res
-            
-        
-    
-    
-# 0 1 2    
+
+
+# Alt
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        order = []
+        def checkEmpty(): return len(matrix) == 0 or len(matrix[0]) == 0
+
+        def goLeft():
+            if checkEmpty():
+                return
+            order.extend(matrix.pop(0))
+            goDown()
+
+        def goDown():
+            if checkEmpty():
+                return
+            order.extend([row.pop(-1) for row in matrix])
+            goRight()
+
+        def goRight():
+            if checkEmpty():
+                return
+            order.extend(reversed(matrix.pop(-1)))
+            goUp()
+
+        def goUp():
+            if checkEmpty():
+                return
+            order.extend(reversed([row.pop(0) for row in matrix]))
+            goLeft()
+
+        goLeft()
+        return order
+
+
+# 0 1 2
 
 # 1 2 3
 # 4 5 6
@@ -50,5 +83,4 @@ class Solution:
 
 # 1 2 3 6 9 8 7 4 5
 
-# 1 2 3 -> 
-    
+# 1 2 3 ->
