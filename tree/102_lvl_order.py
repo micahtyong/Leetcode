f351e28cc31b95bbea4ec42dfa560bd40fe4cf2d
Micah Yong <micah@productiv.com>
2021-07-14 09:06:54 -0700

    Sync

--- tree/102_lvl_order.py
+++ tree/102_lvl_order.py
@@ -9,37 +9,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+from collections import defaultdict
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        levelOrder = {}
+        levelOrder = defaultdict(list)
 
         def helper(root, level):
             # Technique: BFS
             if root == None:
                 return
-            else:
-                levelOrder[level] = levelOrder.get(level, []) + [root.val]
-                helper(root.left, level + 1)
-                helper(root.right, level + 1)
-
-        helper(root, 0)
-        return levelOrder.values()
-
-
-class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        levelOrder = {}
-
-        def helper(root, level):
-            # Technique: BFS
-            if root == None:
-                return
-            else:
-                levelOrder[level] = levelOrder.get(level, []) + [root.val]
-                helper(root.left, level + 1)
-                helper(root.right, level + 1)
+            levelOrder[level].append(root.val)
+            helper(root.left, level + 1)
+            helper(root.right, level + 1)
 
         helper(root, 0)
         return levelOrder.values()
--- tree/104_max_depth_tree.py
+++ tree/104_max_depth_tree.py
@@ -11,5 +11,4 @@ class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if root == None:
             return 0
-        else:
-            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
