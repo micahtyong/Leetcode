34fe80cea8c2c3a9f9e38b398f950366b8f624b7
Micah Yong <micah@productiv.com>
2021-07-31 10:36:15 -0700

    sync

---
+++ graphs/200-num-islands.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/number-of-islands/ 
+
+# Learnings: DFS template 
+# def dfs(self, <..params>): 
+#Base case 
+#Do something
+#Iterate through other possible options and recursively call DFS 
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#' # marks visited islands beforehand
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
